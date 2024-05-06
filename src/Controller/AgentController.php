3a8644f7268274a88698fcbae2d954906d4faa49
Hawkeye <swarup@agpaytech.co.uk>
2024-05-06 20:23:47 +0530

    Project Initial commit

---
+++ src/Controller/AgentController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ *
+ */
+class AgentController extends AppController
+{
+    public function index()
+    {
+
+    }
+}
