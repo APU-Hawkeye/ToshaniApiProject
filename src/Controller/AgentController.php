9730e28ec9afd5d3307fb7379a59443c4ddcb40c
Hawkeye <swarup@agpaytech.co.uk>
2024-05-06 22:13:09 +0530

    methods mentioned

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -3,15 +3,33 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Controller\AppController;
-
-/**
- *
- */
 class AgentController extends AppController
 {
+    /**
+     * @return void
+     */
     public function index()
     {
+    }
 
+    /**
+     * @return void
+     */
+    public function customers()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function addBeneficiary()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function beneficiaryList()
+    {
     }
 }
