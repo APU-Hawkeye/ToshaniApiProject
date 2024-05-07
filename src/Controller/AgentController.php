5c0f81dd432f201c27554dccd89c864afac65d94
Hawkeye <swarup@agpaytech.co.uk>
2024-05-07 21:18:07 +0530

    worked on agent controller

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -3,20 +3,86 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\API\Toshani;
+use Cake\Event\EventInterface;
+
+/**
+ * Class AgentController
+ *
+ * @property \App\Service\API\Toshani $object
+ */
 class AgentController extends AppController
 {
     /**
+     * @param \Cake\Event\EventInterface $event
      * @return void
      */
-    public function index()
+    public function beforeFilter(EventInterface $event)
     {
+        $object = new Toshani(env('SECRET_KEY'));
+        $this->object = $object;
+        parent::beforeFilter($event);
     }
 
     /**
-     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function customers()
+    public function index()
     {
+        if ($this->getRequest()->is('post')) {
+            /** @var int $postData */
+            $postData = $this->getRequest()->getData('mobile_number');
+            /** @var \GuzzleHttp\Psr7\Response $response */
+            $response = $this->object->getCustomer((int)$postData);
+            if ($response->result === 1) {
+                $this->Flash->success($response->message);
+
+                return $this->redirect([
+                    'controller' => 'Agent',
+                    'action' => 'beneficiaryList',
+                    $postData
+                ]);
+            } elseif ($response->result === 0) {
+                $this->Flash->error($response->message);
+            } elseif ($response->result === 2) {
+                $this->Flash->success($response->message);
+
+                return $this->redirect([
+                    'action' => 'validateOTP',
+                    $postData,
+                ]);
+            }
+        }
+        $this->set('titleForLayout', __('Customers'));
+    }
+
+    /**
+     * @param int $mobileNumber Mobile Number
+     * @return \Cake\Http\Response|void|null
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function validateOTP(int $mobileNumber)
+    {
+        if ($this->getRequest()->is('post')) {
+            /** @var int $otp */
+            $otp = $this->getRequest()->getData('otp');
+            /** @var string $name */
+            $name = $this->getRequest()->getData('name');
+            /** @var \GuzzleHttp\Psr7\Response $response */
+            $response = $this->object->validateOTP($mobileNumber, $otp, $name);
+            if ($response['result'] === 1) {
+                $this->Flash->success($response['message']);
+
+                return $this->redirect([
+                    'action' => 'beneficiaryList',
+                    $mobileNumber,
+                ]);
+            } elseif ($response['result'] === 0) {
+                $this->Flash->success($response['message']);
+            }
+        }
+        $this->set('mobile', $mobileNumber);
+        $this->set('titleForLayout', __('OTP Validation for MobileNumber'));
     }
 
     /**
@@ -27,9 +93,21 @@ class AgentController extends AppController
     }
 
     /**
-     * @return void
+     * @param int $mobileNumber Mobile Number
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function beneficiaryList()
+    public function beneficiaryList(int $mobileNumber)
     {
+        /** @var \GuzzleHttp\Psr7\Response $response */
+        $response = $this->object->beneficiaryList($mobileNumber);
+        if ($response->result === 1) {
+            return $response->data;
+        } elseif ($response->result === 0) {
+            $this->Flash->success($response->message);
+        }
+
+        $this->set('response', $response);
+        $this->set('mobile', $mobileNumber);
+        $this->set('titleForLayout', __('Beneficiary List'));
     }
 }
