babe239d9e7538e791482f6ef8bcea9bd766c567
Hawkeye <swarup@agpaytech.co.uk>
2024-05-07 21:19:15 +0530

    route updated

--- config/routes.php
+++ config/routes.php
@@ -65,7 +65,6 @@ return function (RouteBuilder $routes): void {
         /*
          * ...and connect the rest of 'Pages' controller's URLs.
          */
-        $builder->connect('/pages/*', 'Pages::display');
 
         /*
          * Connect catchall routes for all controllers.
