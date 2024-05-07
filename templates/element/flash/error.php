6303d9ffb00ddcb10d86d5054102993702db0e3b
Hawkeye <swarup@agpaytech.co.uk>
2024-05-07 21:18:57 +0530

    flash messages modified

--- templates/element/flash/error.php
+++ templates/element/flash/error.php
@@ -8,4 +8,9 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message error" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<div class="alert alert-danger alert-outline alert-dismissible tx-spacing-2" role="alert">
+    <?php echo $message; ?>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
